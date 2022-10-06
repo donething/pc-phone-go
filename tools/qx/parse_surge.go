f1f24e74e2f695d0910e843be56e69ba12f38c3d
Donething <donething.net@gmail.com>
2022-10-06 20:12:59 +0800

    配置代理访问

--- tools/qx/parse_surge.go
+++ tools/qx/parse_surge.go
@@ -2,32 +2,46 @@ package qx
 
 import (
 	"fmt"
+	"github.com/donething/utils-go/dohttp"
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
+	"pc-phone-conn-go/conf"
 	"pc-phone-conn-go/entity"
 	"pc-phone-conn-go/logger"
 	"strings"
+	"time"
 )
 
 // ParseSurge 解析 surge 分流规则
 func ParseSurge(c *gin.Context) {
+	var client = dohttp.New(120*time.Second, false, false)
+	if conf.Conf.Proxy != "" {
+		err := client.SetProxy(conf.Conf.Proxy, nil)
+		if err != nil {
+			logger.Error.Printf("解析surge分流规则出错，设置网络代理时出错：%s\n", err)
+			c.JSON(http.StatusOK, entity.Rest{Errcode: 5000,
+				Msg: fmt.Sprintf("设置网络代理时出错")})
+			return
+		}
+	}
+
 	// 获取分流配置文件内容
 	var url = c.Query("url")
 	resp, err := http.Get(url)
 	if err != nil {
-		logger.Error.Println(fmt.Sprintf("解析surge分流规则失败，URL有误：'%s'", url))
-		c.JSON(http.StatusOK, entity.Rest{Errcode: 5000,
-			Msg: fmt.Sprintf("解析surge分流规则失败，URL有误：'%s'", url)})
+		logger.Error.Printf("解析surge分流规则出错，请求出错：%s\n", err)
+		c.JSON(http.StatusOK, entity.Rest{Errcode: 5100,
+			Msg: fmt.Sprintf("请求出错")})
 		return
 	}
 	defer resp.Body.Close()
 
 	bs, err := io.ReadAll(resp.Body)
 	if err != nil {
-		logger.Error.Println(fmt.Sprintf("解析surge分流规则失败，无法获取内容：'%s'", url))
-		c.JSON(http.StatusOK, entity.Rest{Errcode: 5000,
-			Msg: fmt.Sprintf("解析surge分流规则失败，无法获取内容：'%s'", url)})
+		logger.Error.Printf("解析surge分流规则出错，无法读取响应内容：'%s'\n", url)
+		c.JSON(http.StatusOK, entity.Rest{Errcode: 5200,
+			Msg: fmt.Sprintf("解析surge分流规则出错，无法读取响应内容：'%s'", url)})
 		return
 	}
 
